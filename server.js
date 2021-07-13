fa62211cd71601c89550f20163c4348cf57908a3
mariocab98 <yankeescab@gmail.com>
2021-07-13 14:14:00 -0400

    updeated server js with mongoose heroku uri

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/fitness-tracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
